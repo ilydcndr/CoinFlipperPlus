5e333ad45ee7f5034f4af2d93dd54c3969ae8e45
ilydcndr <ilaydacundar@hotmail.com>
2019-12-11 15:11:37 +0200

    Updates

---
+++ precache-manifest.4a4316f2422e613dc8df26839a58f1dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816eb91e53ea4061336e092df1e0859e",
+    "url": "/CoinFlipperPlus/index.html"
+  },
+  {
+    "revision": "74b6c363967575e7f334",
+    "url": "/CoinFlipperPlus/static/css/main.5ce09333.chunk.css"
+  },
+  {
+    "revision": "0291d6669f910921206e",
+    "url": "/CoinFlipperPlus/static/js/2.f9f8c388.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/CoinFlipperPlus/static/js/2.f9f8c388.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74b6c363967575e7f334",
+    "url": "/CoinFlipperPlus/static/js/main.1708c7d2.chunk.js"
+  },
+  {
+    "revision": "aa509b64ecd66de3e5f2",
+    "url": "/CoinFlipperPlus/static/js/runtime-main.9a4fda3f.js"
+  },
+  {
+    "revision": "7c42669a738f1a1e15ef7ccedaaa8ed2",
+    "url": "/CoinFlipperPlus/static/media/1-lira-tura.7c42669a.png"
+  },
+  {
+    "revision": "9b70882eb706071cc877c4040c16702d",
+    "url": "/CoinFlipperPlus/static/media/1-lira-yazi.9b70882e.png"
+  }
+]);
